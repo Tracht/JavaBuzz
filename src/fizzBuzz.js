5ecda165d38319ccc95c7079942e4c39e4bb28da
tracht <marija.trachtenberg@gmail.com>
2020-05-12 08:02:04 +0100

    first test passes: returns fizz

---
+++ src/fizzBuzz.js
@@ -0,0 +1,22 @@
+class FizzBuzz {
+
+  play(number) {
+      return 'fizz'
+  }
+
+};
+
+// Original Ruby
+// def fizzbuzz(number)
+//   if number % 5 == 0 && number % 3 == 0
+//     return 'fizzbuzz'
+//   elsif number % 3 == 0
+//     return 'fizz'
+//   elsif number % 5 == 0
+//     return 'buzz'
+//   elsif number > 100 || number < 0
+//     return "out of range"
+//   else
+//     return number
+//   end
+// end
